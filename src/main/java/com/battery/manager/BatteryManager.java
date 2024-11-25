db19d88f395e13b37aa5dd6f8cde1bfd0950d08f
ManuACDev <malamocres@gmail.com>
2024-11-25 12:52:26 +0100

    Creación de la clase principal

---
+++ src/main/java/com/battery/manager/BatteryManager.java
@@ -0,0 +1,10 @@
+package com.battery.manager;
+
+public class BatteryManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Aplicación Battery Manager inicializada");
+	}
+
+}
